122b2b7397199c6294a284d1dbb503c419157133
Lin Chieh <linchieh@stu.comp.nus.edu.sg>
2022-05-14 23:46:13 +0800

    Revert "this is a full length commit message"
    This reverts commit c0347a29176f55dbf0642b83bbc73311428d0eaf.

--- MainTestClass.java
+++
@@ -1,6 +0,0 @@
-public class MainTestClass {
-    public static void main(String[] args) {
-        System.out.println("test method for main branch");
-        System.out.println("made a change to main method");
-    }
-}
