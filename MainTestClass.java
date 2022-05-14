9f9d5d29d76377590893cd2916f2ded061de6bdd
Lin Chieh <linchieh@stu.comp.nus.edu.sg>
2022-05-14 23:22:57 +0800

    Changed MainTestClass on main

--- MainTestClass.java
+++ MainTestClass.java
@@ -1,5 +1,6 @@
 public class MainTestClass {
     public static void main(String[] args) {
         System.out.println("test method for main branch");
+        System.out.println("made a change to main method");
     }
 }
