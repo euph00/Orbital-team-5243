7c33bfe090935c11ce50f2a61574af838df731ed
Lin Chieh <linchieh@stu.comp.nus.edu.sg>
2022-05-14 23:08:12 +0800

    Edit to test class 1 for the first test branch

--- TestClassBranch1.java
+++ TestClassBranch1.java
@@ -1,5 +1,5 @@
 public class TestClassBranch1 {
     public static void main(String[] args) {
-        System.out.println("this is the first test branch");
+        System.out.println("first test branch");
     }
 }
