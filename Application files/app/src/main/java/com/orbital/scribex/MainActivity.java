84de7ccbf4478929e85b8713a6db244a026a28e1
Lin Chieh <linchieh@stu.comp.nus.edu.sg>
2022-05-16 10:52:40 +0800

    commit app test update

--- Application files/app/src/main/java/com/orbital/scribex/MainActivity.java
+++ Application files/app/src/main/java/com/orbital/scribex/MainActivity.java
@@ -3,12 +3,23 @@ package com.orbital.scribex;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button);
+    }
+
+    public void onBtnClick(View view) {
+        Toast.makeText(this, "test", Toast.LENGTH_SHORT).show();
     }
 }
