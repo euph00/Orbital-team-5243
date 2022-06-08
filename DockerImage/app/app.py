1529edc7f61c5fb47642414a1771e336319f3338
NyanYui <nyanyuiyuuki@gmail.com>
2022-06-08 15:39:59 +0900

    troubleshooting

--- DockerImage/app/app.py
+++ DockerImage/app/app.py
@@ -23,21 +23,23 @@ class FBStorage(object):
         doc = self.obj_file.doc_ref.get()
         curr_doc = doc.to_dict()
 
-        update_len = len(self.obj_file.transcribe_imglst())
+        imglst = self.obj_file.transcribe_imglst()
+
+        update_len = len(imglst)
         
         if doc.exists:
             curr_len = len(curr_doc)
             keys = []
             for i in range(curr_len+1,curr_len+update_len+1):
                 keys.append(str(i))
-            upload = dict(zip(keys, self.obj_file.transcribe_imglst()))
+            upload = dict(zip(keys, imglst))
             self.obj_file.doc_ref.update(upload)
         else:
             curr_len = 0
             keys = []
             for i in range(curr_len+1,curr_len+update_len+1):
                 keys.append(str(i))
-            upload = dict(zip(keys, self.obj_file.transcribe_imglst()))
+            upload = dict(zip(keys, imglst))
             self.obj_file.doc_ref.set(upload)
 
 
@@ -59,16 +61,15 @@ class File(object):
 
     def transcribe_imglst(self):
         new_entries = []
-        
+
         for image in self.get_imagelist()[1:]:
             #   Retrieve image from firebase storage and converting it into a image object for ocr
             blob = self.bucket.get_blob(image)
             arr = np.frombuffer(blob.download_as_string(), np.uint8)
             Timage = cv2.imdecode(arr,cv2.COLOR_BGR2BGR555)
-
+            
             #  Transcribe
             transcribedfilename = ocrfn.turn_img_into_text(Timage)
-
             #  Upload result txt file into firebase storage
             outputblob = self.bucket.blob(transcribedfilename)
             outputblob.upload_from_string(transcribedfilename)
@@ -98,6 +99,9 @@ class App(object):
     def ping(self):
         teststorage = FBStorage(self.cred, self.app, 'imgfiles.txt')
         teststorage.upload_result()
+
+cred = credentials.Certificate('./app/keySX.json')
+Fapp = firebase_admin.initialize_app(cred, {'storageBucket' : 'scribex-1653106340524.appspot.com'})
 
 def main():
     test_app = App()
