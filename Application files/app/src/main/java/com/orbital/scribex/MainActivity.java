8a6e7d46ff517e026c2c6e2b21fbfeaa7165955b
NyanYui <nyanyuiyuuki@gmail.com>
2022-06-19 05:42:30 +0900

    updated design

--- Application files/app/src/main/java/com/orbital/scribex/MainActivity.java
+++ Application files/app/src/main/java/com/orbital/scribex/MainActivity.java
@@ -5,6 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -43,6 +45,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
 
         //GSO object for configuring scope of requested info
